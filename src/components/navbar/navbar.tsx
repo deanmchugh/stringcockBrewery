c92b58519392465e683aa68ddae287c0fd2d53a8
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-01 14:15:14 +1000

    types

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,14 +1,16 @@
 import React from 'react'
 
 import './navbar.scss'
+import menuImage from './assets/menuClosed.svg'
 
 export function NavBar() {
     return (
         <div className='navBody'>
             <div className='navContent'>
-                history
-                products
-                merchendise
+                <img src={menuImage} alt='menu image' />
+                <p>history</p>
+                <p>products</p>
+                <p>merchandise</p>
             </div>
         </div>
     )
