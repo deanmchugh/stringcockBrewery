ef088910bdb3329f4e9221b6bb2138b288667c4b
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-01 21:32:46 +1000

    banner

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import './navbar.scss'
 import menuImage from './assets/menuClosed.svg'
 
-export function NavBar() {
+export const NavBar = () => {
+    const [styling, setStyling] = useState('navShow')
+
+    function toggleMenu() {
+        const state = styling === 'navShow' ? 'navHide' : 'navShow'
+        setStyling(state)
+    }
+
     return (
         <div className='navBody'>
-            <div className='navContent'>
-                <img src={menuImage} alt='menu image' />
-                <p>history</p>
-                <p>products</p>
-                <p>merchandise</p>
+            <button onClick={() => toggleMenu()}>
+                    <img src={menuImage} alt='menu' />
+            </button>
+            <div className={styling}>
+                <p>History</p>
+                <p>Products</p>
+                <p>Merchandise</p>
+                <p>Contact</p>
             </div>
         </div>
     )
