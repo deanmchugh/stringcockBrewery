d8053ead94ce8cdf307a69cb627a9a524c179240
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-08 10:46:44 +1000

    banners

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -1,13 +1,22 @@
 import React from 'react'
+import CSS from 'csstype'
 import './banner.scss' 
 
-import magic from './magic.png'
+import magic from './assets/magic.png'
+import sword from './assets/crossed-swords.png'
+import potion from './assets/potion.png'
 
-export const Banner = () => {
+export const Banner = (image: string, colour: string) => {
+    
+    const borderColour: CSS.Properties = {
+        borderLeft: 'solid 20px rgba(' + {colour} + ');',
+        borderRight: 'solid 20px rgba(' + {colour} + ');'
+    }
+    
     return (
         <div className='banner'>
-            <div className='border'>
-                <img className='image' src={magic} />
+            <div className='border' style={borderColour} >
+                <img className='image' src={image} />
             </div>
         </div>
     )
--- src/index.tsx
+++ src/index.tsx
@@ -6,11 +6,16 @@ import './scss/index.scss'
 import {NavBar} from './components/navbar/navbar'
 import {Banner} from './components/banner/banner'
 
+import magic from '../src/components/banner/assets/magic.png'
+import sword from '../src/components/banner/assets/crossed-swords.png'
+import potion from '../src/components/banner/assets/potion.png'
+
 function Main() {
     return (
         <div className='landingPage'>
-            <NavBar />
-            <Banner />
+            <Banner image={magic} colour={'255, 0, 255, 0.5'}/>
+            <Banner image={sword} colour={'255, 0, 0, 0.5'}/>
+            <Banner image={potion} colour={'0, 0, 255, 0.5'}/>
         </div>
     )
 }
