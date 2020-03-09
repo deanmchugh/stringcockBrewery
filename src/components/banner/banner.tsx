52f5aa21aef4d6788685c9f2c02f2b83a335b6bc
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-09 21:18:44 +1000

    banner

--- src/components/banner/banner.tsx
+++ src/components/banner/banner.tsx
@@ -6,18 +6,16 @@ import magic from './assets/magic.png'
 import sword from './assets/crossed-swords.png'
 import potion from './assets/potion.png'
 
-export const Banner = (image: string, colour: string) => {
-    
-    const borderColour: CSS.Properties = {
-        borderLeft: 'solid 20px rgba(' + {colour} + ');',
-        borderRight: 'solid 20px rgba(' + {colour} + ');'
-    }
+type Props = {
+    image: string;
+    colour: string
+}
+
+export const Banner = ({image, colour}: Props) => {
     
     return (
         <div className='banner'>
-            <div className='border' style={borderColour} >
-                <img className='image' src={image} />
-            </div>
+            <img className='image' src={image} />
         </div>
     )
 }
