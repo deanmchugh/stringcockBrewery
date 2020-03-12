dfd4994f9b547a37352bc550e89bdcb087669762
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-12 20:53:17 +1000

    bio

---
+++ src/components/bio/Bio.tsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import './bio.scss'
+
+export const Bio = () => {
+    return (
+        <div className='bio'>
+            <p className='firstPara'>
+                Strongcock Brewery is the foundry of M. Strong a Master Alchemist with 
+                an eternal quest to create the ultimate beer, liquid gold. The 
+                journey required the transmutation of the purest ingredients at the 
+                cost of many heroes sacrificing their lives to bring back these 
+                components.
+            </p>
+            <p>
+                Water stolen from the everflowing goblet of Posiden while he sat to feast 
+                in the center of his kingdom. 
+            </p>
+            <p>
+                The malt was given as a prize by Demeter for a race against the rising 
+                sun across her eternal field of grains. Dried with the fires of an 
+                elder red dragon. A single flower painstakingly picked from the hop 
+                vine in her personal garden each full moon and accumulated over a 
+                lifetime.
+            </p>
+            <p>
+                The descendants of Dionysus carried with them his recipe for the 
+                perfect beer along with the mother Yeast from the very first 
+                brewing. Convinced only to share these secrets through a drinking 
+                competition completed over the period of a solar year.
+            </p>
+        </div>
+    )
+}
--- src/index.tsx
+++ src/index.tsx
@@ -5,17 +5,12 @@ import './scss/index.scss'
 
 import {NavBar} from './components/navbar/navbar'
 import {Banner} from './components/banner/banner'
-
-import magic from '../src/components/banner/assets/magic.png'
-import sword from '../src/components/banner/assets/crossed-swords.png'
-import potion from '../src/components/banner/assets/potion.png'
+import {Bio} from './components/bio/Bio'
 
 function Main() {
     return (
         <div className='landingPage'>
-            <Banner img={magic} clr={'blue'} name={'THE WIZARD'} desc={'A Radler with fruity finish'}/>
-            <Banner img={sword} clr={'red'} name={'THE WARRIOR'} desc={'A India Pale Larger with a bold flavour'}/>
-            <Banner img={potion} clr={'green'} name={'THE HEALER'} desc={'A refreshing IPA'}/>
+            <Bio />
         </div>
     )
 }
