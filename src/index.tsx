db32c614745fdf39c6b1361720d22447d625a883
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-13 20:20:45 +1000

    wax seal

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 import './scss/index.scss'
+import waxStamp from './waxStamp.png'
 
 import {NavBar} from './components/navbar/navbar'
 import {Banner} from './components/banner/banner'
@@ -10,7 +11,8 @@ import {Bio} from './components/bio/Bio'
 function Main() {
     return (
         <div className='landingPage'>
-            <Bio />
+            <NavBar />
+            <img className='waxStamp' src={waxStamp} alt='wax stamp' />
         </div>
     )
 }
