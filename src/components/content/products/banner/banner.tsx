b4755aacc7aa4f1d68207b40e7acacee321324d8
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-21 15:37:50 +1000

    expanding banner

--- src/components/content/products/banner/banner.tsx
+++ src/components/content/products/banner/banner.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {useState} from 'react'
 import './banner.scss' 
 
 type Props = {
@@ -9,6 +9,7 @@ type Props = {
 }
 
 export const Banner = ({img, clr, name, desc}: Props) => {
+    const [display, setDisplay] = useState('compact')
 
     const borderStyle = {
         filter: `drop-shadow(3px 3px 0 ${clr}) 
@@ -17,9 +18,16 @@ export const Banner = ({img, clr, name, desc}: Props) => {
                 drop-shadow(-3px -3px 0 ${clr}) 
                 drop-shadow(5px 5px 3px black)`
     }
+
+    const displayInfo = () => {
+        display === 'compact' ? setDisplay('banner') : setDisplay('compact')
+    }
     
     return (
-        <div className='banner' style={borderStyle}>
+        <div className={display} 
+             style={borderStyle}
+             onClick={() => displayInfo()}
+        >
             <img className='image' src={img} />
             <h2 className='name'>{name}</h2>
             <h4 className='desc'>{desc}</h4>
