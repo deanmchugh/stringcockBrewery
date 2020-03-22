9dda5da57ad384f138c83f374950dc76f542e7a7
Dean McHugh <dean.mchugh1988@gmail.com>
2020-03-23 07:55:04 +1000

    styling

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -13,18 +13,14 @@ export const NavBar = () => {
         setStyling(state)
     }
 
-    const highlight = {
-        borderBottom: content === HTMLElement.name ? '3px solid red' : '0 solid red'
-    }
-
     return (
         <div className='navBody'>
             <div className={styling}>     
-                <button name='history' style={highlight} onClick={() => setContent('history')}>History</button>
-                <button name='product' style={highlight} onClick={() => setContent('product')}>Products</button> 
+                <button id='history' onClick={() => setContent('history')}>History</button>
+                <button id='product' onClick={() => setContent('product')}>Products</button> 
                 <img className='logo' src={logo} alt='logo' /> 
-                <button name='merch' style={highlight} onClick={() => setContent('merch')}>Merchandise</button>
-                <button name='inquire' style={highlight} onClick={() => setContent('contact')}>Inquire</button> 
+                <button id='merch' onClick={() => setContent('merch')}>Merchandise</button>
+                <button id='inquire' onClick={() => setContent('contact')}>Inquire</button> 
             </div>
             <Content content={content}/>
         </div>
