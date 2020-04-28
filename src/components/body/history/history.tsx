cac11d1341966ba9de03cbabdfbcf8830f2f15b7
Dean McHugh <dean.mchugh1988@gmail.com>
2020-04-28 18:34:34 +1000

    styling

--- src/components/body/history/history.tsx
+++ src/components/body/history/history.tsx
@@ -14,11 +14,11 @@ export const History = () => {
                     components.
                 </p>
                 <p>
-                    Water stolen from the everflowing goblet of Posiden while he sat to feast 
+                    Water stolen from the everflowing goblet of Poseidon, God of the sea, while he sat to feast 
                     in the center of his kingdom. 
                 </p>
                 <p>
-                    The malt was given as a prize by Demeter for a race against the rising 
+                    The malt was given as a prize by Demeter, God of harvest, for a race against the rising 
                     sun across her eternal field of grains. Dried with the fires of an 
                     elder red dragon. 
                 </p>
@@ -28,7 +28,7 @@ export const History = () => {
                     lifetime.
                 </p>
                 <p>
-                    The descendants of Dionysus carried with them his recipe for the 
+                    The descendants of Dionysus, God of festivity, carried with them his recipe for the 
                     perfect beer along with the mother Yeast from the very first 
                     brewing. Convinced only to share these secrets through a drinking 
                     competition completed over the period of a solar year.
